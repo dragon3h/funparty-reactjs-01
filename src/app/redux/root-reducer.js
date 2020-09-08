6f64c3965456ff5b54c7ddba5321f237580978a0
dragon3hgmx <dragon3h@gmx.com>
2020-09-08 19:08:48 -0400

    commit before deploying to Heroku

--- src/app/redux/root-reducer.js
+++ src/app/redux/root-reducer.js
@@ -1,11 +1,21 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import bouncyCastlesReducer from './products/bouncy-castles/bouncy-castles.reducer';
 import bouncyCastleDetailsReducer from './products/bouncy-castle-details/bouncy-castle-details.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['bouncyCastleDetails'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   bouncyCastles: bouncyCastlesReducer,
   bouncyCastleDetails: bouncyCastleDetailsReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider, connect} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import styles from './index.module.scss';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
-import store from './app/redux/store';
+import {store, persistor} from './app/redux/store';
 
 ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
         <BrowserRouter>
-          <App/>
+          <PersistGate persistor={persistor}>
+            <App/>
+          </PersistGate>
         </BrowserRouter>
       </Provider>
     </React.StrictMode>,
